b0b1330cb335a9c6de4086a2a27e43796efa1b3c
coskunbaygar <coskun.baygar@gmail.com>
2026-04-30 09:04:07 +0100

    Converted TicketTypeRequest object to full immutable

--- src/main/java/uk/gov/dwp/uc/pairtest/domain/TicketTypeRequest.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/domain/TicketTypeRequest.java
@@ -4,10 +4,13 @@ package uk.gov.dwp.uc.pairtest.domain;
  * Immutable Object
  */
 
-public class TicketTypeRequest {
+/*
+* The final keyword was missing from the class and variable declarations; it has been added to ensure the object is immutable.
+*/
+public final class TicketTypeRequest {
 
-    private int noOfTickets;
-    private Type type;
+    private final int noOfTickets;
+    private final Type type;
 
     public TicketTypeRequest(Type type, int noOfTickets) {
         this.type = type;
